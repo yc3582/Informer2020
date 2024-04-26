1c77e2954d08d777c9a702a256802ce086a78f7f
bruce-stari <bruce@stari.io>
2024-04-26 17:38:56 -0400

    changed data loader

--- data/data_loader.py
+++ data/data_loader.py
@@ -47,8 +47,10 @@ class Dataset_ETT_hour(Dataset):
         df_raw = pd.read_csv(os.path.join(self.root_path,
                                           self.data_path))
 
-        border1s = [0, 12*30*24 - self.seq_len, 12*30*24+4*30*24 - self.seq_len]
-        border2s = [12*30*24, 12*30*24+4*30*24, 12*30*24+8*30*24]
+        #border1s = [0, 12*30*24 - self.seq_len, 12*30*24+4*30*24 - self.seq_len]
+        #border2s = [12*30*24, 12*30*24+4*30*24, 12*30*24+8*30*24]
+        border1s = [0,        200*24*6 - self.seq_len, 200*24*6 - self.seq_len]
+        border2s = [200*24*6, 200*24*6+45*24*6,        200*24*6+45*24*6]
         border1 = border1s[self.set_type]
         border2 = border2s[self.set_type]
         
